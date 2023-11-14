143576e2d5639152e28213b660c513074fa48a20
sungyeah <98869025+sungyeah@users.noreply.github.com>
2023-11-14 16:17:50 +0900

    한글 변환 함수 생성 및 outputbox 추가

--- source/common.js
+++ source/common.js
@@ -2,16 +2,23 @@
 유니코드 이스케이프 시퀀스 함수 정의
 (\\u) 다음에 나오는 4자리의 16진수를 찾아, 해당 코드 포인트를 가진 문자로 변환
 */
+
+/*
+  이스케이프 시퀀스 to 한글 변환 함수 정의
+*/
 function decodeUnicodeEscapes(str) {
-  return str.replace(/\\[\dA-Fa-f]{4}/g, (match) => String.fromCharCode(parseInt(match.substr(2), 16)));
+  const decodedString = eval(`"${str}"`);
+  const koreanString = decodeURIComponent(escape(decodedString));
+  return koreanString
 }
 
 /*
 데이터 딕셔너리 변환 함수 정의
 */
 function setData() {
-  let txt = document.getElementById('txtBox').value;
-  let arr = txt.split('name');
+  let inputTxt = document.getElementById('inputBox').value;
+  let outPutTxt = document.getElementById('outPutBox');
+  let arr = inputTxt.split('name');
   let data = {};
   for (var i of arr) {
     let splitQuote = i.split('"');
@@ -19,29 +26,26 @@ function setData() {
       if (splitQuote[1] == '_sn') {
         if (splitQuote[3].includes('\\')) {
           var value = splitQuote[3];
-          var replaceValue = String(value.replace(/\\/g, '\\'));
-          console.log(replaceValue);
-          let decodeValue = decodeURIComponent(escape(replaceValue));
-          console.log(decodeValue);
+          let decodeValue = decodeUnicodeEscapes(value);
           data['screen_name'] = decodeValue;
+        } else {
+          data['screen_name'] = splitQuote[3];
         }
-        data['screen_name'] = splitQuote[3];
       } else if (splitQuote[1] == '_sc') {
         data['screen_class'] = splitQuote[3];
       } else if (splitQuote[1].includes('ep_') || splitQuote[1].includes('up_') || splitQuote[1].includes('dimension')) {
         if (splitQuote[3].includes('\\')) {
           var value = splitQuote[3];
-          var replaceValue = String(value.replace(/\\/g, '\\'));
-          console.log(replaceValue);
-          let decodeValue = decodeURIComponent(escape(replaceValue));
-          console.log(decodeValue);
+          let decodeValue = decodeUnicodeEscapes(value);
           data[splitQuote[1]] = decodeValue;
+        } else {
+          data[splitQuote[1]] = splitQuote[3];
         }
-        data[splitQuote[1]] = splitQuote[3];
       } else if (splitQuote[1].includes('cm_') || splitQuote[1].includes('metric')) {
         data[splitQuote[1]] = splitQuote[3];
       }
     }
   }
   console.log(data);
+  outPutTxt.value = JSON.stringify(data);
 }
