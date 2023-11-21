b34a60233ce32a42023e6e854e088be3332fefd5
sungyeah <98869025+sungyeah@users.noreply.github.com>
2023-11-22 08:40:28 +0900

    viewList 함수 구현

--- source/common.js
+++ source/common.js
@@ -1,3 +1,26 @@
 function setData() {
   convertJson();
+  viewList();
+}
+
+function viewList() {
+  const eventTag = document.getElementById('eventList');
+
+  for(i in events) {
+    const timestamp = Number(events[i].remainDatas.start_timestamp_millis)
+
+    const date = new Date(timestamp)
+    const formattedDate = date.toISOString().replace('T', ' ').substring(0, 19);
+
+    eventTag.insertAdjacentHTML('beforeend',
+      `<div class="eventSummary" onclick="viewEvent(${i})">
+       <div class="evnetNo">${Number(i+1)}</div>
+       <div class="eventName">${events[i].eventName}</div>
+       <div class="time">${formattedDate}</div>
+       </div>`)
+  }
+}
+
+function viewEvent(no) {
+  
 }
